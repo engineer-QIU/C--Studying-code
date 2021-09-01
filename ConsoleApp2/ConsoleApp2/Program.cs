3c60b307fffced3d1572ce207e624421bd9de904
engineer-QIU <2311674683@qq.com>
2021-09-01 10:48:13 +0800

    提交代码

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入你的名字");
+            string str = Console.ReadLine();
+            Console.WriteLine("你的名字是{0}", str);
+            Console.ReadKey();
+        }
+    }
+}
